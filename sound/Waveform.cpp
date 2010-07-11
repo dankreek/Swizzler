0979f40ed9c77ce0f21fcb4ac73589353a60f2dc
justin <justin@blaptop>
2010-07-11 11:21:59 -0700

    Waveform generation completed

--- sound/Waveform.cpp
+++ sound/Waveform.cpp
@@ -7,6 +7,49 @@
 
 #include "Waveform.h"
 
-Waveform::Waveform() {
+uint8_t Waveform::pulseWidth;
+int8_t Waveform::triangleBuffer[waveformLength];
+int8_t Waveform::squareBuffer[waveformLength];
+int8_t Waveform::sawtoothBuffer[waveformLength];
 
+Waveform::Waveform() {
+  // Make triangle the default waveform
+  curWave = triangleBuffer;
+}
+
+void Waveform::init() {
+  // Set pulse width to 50% (and redraw waveform)
+  setPulseWidth(64);
+  generateSawtoothWave();
+  generateTriangleWave();
+}
+
+void Waveform::generateSawtoothWave() {
+  int8_t val = -128;
+  for (int i=0; i < waveformLength; i++) {
+    sawtoothBuffer[i] = val;
+    val += 2;
+  }
+}
+
+void Waveform::setPulseWidth(uint8_t pw) {
+  pulseWidth = pw;
+  for (int i=0; i < waveformLength; i++) {
+    squareBuffer[i] = (i < pulseWidth) ? -128 : 127;
+  }
+}
+
+void Waveform::generateTriangleWave() {
+  int8_t val=-128;
+  uint8_t i;
+
+  for (i=0; i < waveformLength/2; i++) {
+    triangleBuffer[i] = val;
+    val += 4;
+  }
+
+  for (; i < waveformLength; i++) {
+    triangleBuffer[i] = val;
+    val -= 4;
+  }
 }
--- sound/Waveform.h
+++ sound/Waveform.h
@@ -8,6 +8,8 @@
 #ifndef WAVEFORM_H_
 #define WAVEFORM_H_
 
+#include <inttypes.h>
+
 enum WaveformType {
   triangleWave,
   squareWave,
@@ -17,21 +19,33 @@ enum WaveformType {
 
 class Waveform {
 public:
-  // Generate the specified wave form
-  static void generateWaveforms();
+  Waveform();
+
+  // Generate the wavetables
+  static void init();
 
   // Length of a wavetable
-  // TODO : Make this smaller! Need to fit more waves in memory at one time
-  static const uint8_t waveformLength = 256;
+  static const uint8_t waveformLength = 128;
 
   // Buffers which contain wavetables
   static int8_t triangleBuffer[waveformLength];
   static int8_t squareBuffer[waveformLength];
-  static int8_t pulseBuffer[waveformLength];
   static int8_t sawtoothBuffer[waveformLength];
 
-  // Pointer to the current wavetable
-  int8_t *curWavetable;
+  /**
+   * Set (and redraw) the pulse width of the pulse wave (7 bits are significant)
+   */
+  static void setPulseWidth(uint8_t pw);
+
+private:
+  // Pointer to this instance's current wavetable
+  int8_t *curWave;
+
+  // Generate the wave forms in memory
+  static void generateTriangleWave();
+  static void generateSawtoothWave();
+
+  static uint8_t pulseWidth;
 };
 
 #endif /* WAVEFORM_H_ */
