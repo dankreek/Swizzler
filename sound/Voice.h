62f550cd6b29de04df9eb4591593e17fe7d059a4
Justin May <jmay@hoser>
2010-07-21 17:24:09 -0700

    Follow this example

--- sound/Voice.h
+++ sound/Voice.h
@@ -44,6 +44,9 @@ public:
 
   uint16_t phaseAccumulator;
 
+  // Apply power-on defaults
+  void resetVoice();
+
 private:
   // The rate at which the phase accumulator increments
   uint16_t phaseChangeRate;
