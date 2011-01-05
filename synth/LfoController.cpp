18cebc96926621dd4f55dea4786363b42ac40624
justin <justin@blaptop>
2011-01-04 21:54:24 -0800

    WHoops

---
+++ synth/LfoController.cpp
@@ -0,0 +1,29 @@
+/*
+ * LfoController.cpp
+ *
+ *  Created on: Jan 4, 2011
+ *      Author: justin
+ */
+#include "LfoController.h"
+#include "Swizzler.h"
+
+void LfoController::setLfoMode(LfoMode mode) {
+  switch (mode) {
+    case frequencyModulation:
+      Swizzler::oscillators.setFrequencyModulation(true);
+      break;
+    case amplitudeModulation:
+      Swizzler::oscillators.setFrequencyModulation(false);
+      break;
+    case freqAndPwModulation:
+      Swizzler::oscillators.setFrequencyModulation(true);
+      break;
+    case pulsewidthModulation:
+      Swizzler::oscillators.setFrequencyModulation(false);
+      break;
+  }
+}
+
+void LfoController::nextTick() {
+
+}
---
+++ synth/LfoController.h
@@ -0,0 +1,31 @@
+/*
+ * LfoController.h
+ *
+ *  Created on: Jan 1, 2011
+ *      Author: justin
+ */
+
+#ifndef LFOCONTROLLER_H_
+#define LFOCONTROLLER_H_
+
+#include "ITimerCall.h"
+
+/**
+ * This class contains all the code that controls which LFO effects are currently
+ * enabled.
+ */
+class LfoController : public ITimerCall {
+public:
+  enum LfoMode {
+    frequencyModulation,
+    pulsewidthModulation,
+    freqAndPwModulation,
+    amplitudeModulation
+  };
+
+  void setLfoMode(LfoMode mode);
+
+  void nextTick();
+};
+
+#endif /* LFOCONTROLLER_H_ */
