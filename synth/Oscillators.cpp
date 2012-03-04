907b37d389947c1dc241ed23dece77674866a873
Justin May <may.justin@gmail.com>
2012-03-03 20:10:54 -0800

    Documentation

--- synth/Oscillators.cpp
+++ synth/Oscillators.cpp
@@ -1,8 +1,5 @@
-/*
- * Oscillators.cpp
- *
- *  Created on: Aug 28, 2010
- *      Author: justin
+/** @file Oscillators.cpp
+ *  @date Aug 28, 2010
  */
 
 #include "Oscillators.h"
@@ -10,38 +7,40 @@
 #include "FreqUtils.h"
 
 Oscillators::Oscillators() {
-  curRootNote = 0;
-  for (uint8_t i=0; i < numVoices; i++) {
-    noteOffset[i] = 0;
-    lastFreq[i] = 0;
-  }
+	_curRootNote = 0;
+	for (uint8_t i=0; i < VCO_COUNT; i++) {
+		_noteOffset[ i ] = 0;
+		_lastFreq[ i ] = 0;
+	}
 }
 
-void Oscillators::noteOn(uint8_t noteNumber, uint8_t velocity) {
-  curRootNote = noteNumber;
-  update();
+void Oscillators::noteOn( uint8_t noteNumber, uint8_t velocity ) {
+	_curRootNote = noteNumber;
+	update();
 }
 
 void Oscillators::update() {
-  for (uint8_t i=0; i < numVoices; i++) { outputFrequency(i); }
+	// Update the output frequency for each oscillator
+	for ( uint8_t i=0; i < VCO_COUNT; i++ ) { _outputFrequency( i ); }
 }
 
-void Oscillators::outputFrequency(uint8_t voiceNum) {
-  int16_t freqModOffset = Swizzler::freqModEffectChain.getFreqOffset();
-  uint16_t outFreq = FreqUtils::modulatedFreq(curRootNote + noteOffset[voiceNum], freqModOffset);
+void Oscillators::_outputFrequency( uint8_t voiceNum ) {
+	// TODO : Find out why this isn't being done by the Frequency filter chain
+	int16_t freqModOffset = Swizzler::freqModEffectChain.getFreqOffset();
+	uint16_t outFreq = FreqUtils::modulatedFreq( _curRootNote + _noteOffset[voiceNum], freqModOffset );
 
-  if (outFreq != lastFreq[voiceNum]) {
-    Swizzler::soundChip.setFrequency(voiceNum, outFreq);
-    lastFreq[voiceNum] = outFreq;
-  }
+	if (outFreq != _lastFreq[voiceNum]) {
+		Swizzler::soundChip.setFrequency( voiceNum, outFreq );
+		_lastFreq[ voiceNum ] = outFreq;
+	}
 }
 
-void Oscillators::noteOff(uint8_t noteNumber) {
+void Oscillators::noteOff( uint8_t noteNumber ) {
   // We really don't need to do any note off stuff here, since the gate stuff's already been done
 }
 
-void Oscillators::setNoteOffset(uint8_t oscNum, int8_t offset) {
-  noteOffset[oscNum] = offset;
-  outputFrequency(oscNum);
+void Oscillators::setNoteOffset( uint8_t oscNum, int8_t offset ) {
+	_noteOffset[ oscNum ] = offset;
+	_outputFrequency( oscNum );
 }
 
--- synth/Oscillators.h
+++ synth/Oscillators.h
@@ -1,12 +1,5 @@
-/*
- * Oscillators.h
- *
- *  Created on: Aug 28, 2010
- *      Author: justin
- *
- *
- * This class takes the root note that's been played on the keyboard and
- * sends it to each of the 3 oscillator's frequency filters.
+/** @file Oscillators.h
+ *  @date Aug 28, 2010
  */
 
 #ifndef OSCILLATORS_H_
@@ -16,32 +9,65 @@
 #include "FreqFilterChain.h"
 #include "ITimerCall.h"
 
+/**
+ * This class takes the root note that's been played on the keyboard and
+ * sends it to each of the 3 oscillator's frequency filters.
+ *
+ * @author Justin May <may.justin@gmail.com>
+ */
 class Oscillators : public ANoteReceiver {
 public:
-  Oscillators();
-  void noteOn(uint8_t noteNum, uint8_t velocity);
-  void noteOff(uint8_t noteNum);
+	/// Initialize this class
+	Oscillators();
 
-  // Set an oscillator's note offset
-  void setNoteOffset(uint8_t oscNum, int8_t offset);
+	/**
+	 * Receive a note on command from the MIDI note filter chain, then output
+	 * the correct frequencies to each oscillator.
+	 *
+	 * @param noteNum	The MIDI note number received from the keyboard
+	 * @param velocity	The velocity that the note was struck at
+	 */
+	void noteOn(uint8_t noteNum, uint8_t velocity);
 
-  // Signal an update to all oscillators
-  void update();
 
-  // The number of voices with frequency (voice 4 is only white noise)
-  static const uint8_t numVoices = 3;
+	/// No action needed on a note off command
+	void noteOff(uint8_t noteNum);
+
+	/**
+	 * Set an oscillator's note offset
+	 *
+	 * @param oscNum	Oscillator number to set note offset for
+	 * @param offset	Number of half-steps (+ or -) to offset the oscillator's frequency by
+	 */
+	void setNoteOffset(uint8_t oscNum, int8_t offset);
+
+	/**
+	 * Send out the current frequency to each oscillator on the sound chip.
+	 */
+	void update();
+
+	/// The number of voices with frequency (voice 4 is only white noise)
+	static const uint8_t VCO_COUNT = 3;
 
 private:
-  uint8_t curRootNote;
+	/// The note that was last sent by the MIDI note filter chain
+	uint8_t _curRootNote;
 
 //  FreqFilterChain oscillatorList[numVoices];
-  int8_t noteOffset[numVoices];
 
-  // Keep track of the last frequency output so no redundant frequency is sent to the sound chip
-  uint16_t lastFreq[numVoices];
+	/// Note offset (in half-steps) for each oscillator
+	int8_t _noteOffset[VCO_COUNT];
 
-  // Calculate the frequency and send out to the sound chip
-  void outputFrequency(uint8_t voiceNum);
+	/// Keep track of the last frequency output so no redundant frequency update is sent to the sound chip
+	uint16_t _lastFreq[VCO_COUNT];
+
+	/**
+	 * Calculate the frequency and send it out to the sound chip. The frequency
+	 * is only sent if the frequency is different than the last one sent.
+	 *
+	 * @param voiceNum	Oscillator number to attempt to send the frequency to
+	 */
+	void _outputFrequency(uint8_t voiceNum);
 };
 
 #endif /* OSCILLATORS_H_ */
